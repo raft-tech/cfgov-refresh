e4f1a154409323de2b6959c0c8be8eb9eeaa9864
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2019-09-12 15:15:08 -0700

    Narrow filters by other applied filters.

--- cfgov/prepaid_agreements/views.py
+++ cfgov/prepaid_agreements/views.py
@@ -26,20 +26,30 @@ def validate_page_number(request, paginator):
     return page_number
 
 
-def get_available_filters(products):
-    available_filters = {'prepaid_type': [], 'status': [], 'issuer_name': []}
+def get_available_filter_options(products, filter_name):
+    filter_options = set()
     for product in products:
-        prepaid_type = product.prepaid_type
-        if prepaid_type and prepaid_type != '':
-            if prepaid_type not in available_filters['prepaid_type']:
-                available_filters['prepaid_type'].append(prepaid_type)
-        status = product.status
-        if status and status not in available_filters['status']:
-            available_filters['status'].append(status)
-        issuer_name = product.issuer_name
-        if issuer_name and issuer_name not in available_filters['issuer_name']:
-            available_filters['issuer_name'].append(issuer_name)
-    return available_filters
+        filter_value = getattr(product, filter_name)
+        filter_options.add(filter_value)
+    return sorted(filter_options)
+
+
+def get_available_filters(filtered_sets, products, valid_filters):
+    available_filter_options = {}
+    applied_filters = filtered_sets.keys()
+    for filter_name in valid_filters:
+        other_applied_filters = [f for f in applied_filters if f != filter_name]
+        if len(other_applied_filters) > 0:
+            product_set = filtered_sets[other_applied_filters.pop(0)]
+            for additional_filter in other_applied_filters:
+                product_set = product_set & filtered_sets[additional_filter]
+            available_filter_options[filter_name] = \
+                get_available_filter_options(product_set, filter_name)
+        else:
+            available_filter_options[filter_name] = \
+                get_available_filter_options(products.all(), filter_name)
+
+    return available_filter_options
 
 
 def search_products(search_term, search_field, products):
@@ -62,34 +72,35 @@ def search_products(search_term, search_field, products):
 
 
 def filter_products(filters, products):
+    filtered_sets = {}
     if 'issuer_name' in filters:
         issuers = Q()
         for issuer in filters['issuer_name']:
-            issuers |= Q(issuer_name__iexact=issuer)
-        products = products.filter(issuers)
+            issuers |= Q(issuer_name__iexact=issuer.strip(' '))
+        filtered_sets['issuer_name'] = products.filter(issuers)
 
     if 'prepaid_type' in filters:
         prepaid_types = Q()
         for prepaid_type in filters['prepaid_type']:
-            prepaid_types |= Q(prepaid_type__iexact=prepaid_type)
-        products = products.filter(prepaid_types)
+            prepaid_types |= Q(prepaid_type__iexact=prepaid_type.title())
+        filtered_sets['prepaid_type'] = products.filter(prepaid_types)
 
     if 'status' in filters:
-        products = products.filter(status__iexact=filters['status'][0])
+        filtered_sets['status'] = products.filter(status__iexact=filters['status'][0])
 
-    return products
+    return filtered_sets
 
 
 def index(request):
     params = dict(request.GET.iterlists())
-    available_filters = {}
+    valid_filters = ['prepaid_type', 'status', 'issuer_name']
+    filters = {f: params[f] for f in valid_filters if f in params}
     search_term = None
     search_field = None
     products = PrepaidProduct.objects
     total_count = products.count()
-    valid_filters = [
-        'prepaid_type', 'status', 'issuer_name'
-    ]
+    available_filter_options = {}
+
     if params:
         params.pop('page', None)
         search_term = params.pop('q', None)
@@ -102,30 +113,40 @@ def index(request):
                 products = search_products(
                     search_term, search_field, products
                 )
-            available_filters = get_available_filters(products)
-        products = filter_products(params, products)
 
-    if not available_filters:
+        # use filtered sets to get available options for each of the valid
+        # filter types
+        filtered_sets = filter_products(filters, products)
+        applied_filters = filtered_sets.keys()
+        if len(applied_filters) > 0:
+            available_filter_options = get_available_filters(
+                                    filtered_sets, products, valid_filters)
+            # get the intersection of the filtered sets of products
+            products = filtered_sets[applied_filters.pop(0)]
+            for additional_filter in applied_filters:
+                products = products & filtered_sets[additional_filter]
+
+    if not available_filter_options:
+        all_products = products.all()
         for filter_name in valid_filters:
-            available_filters[filter_name] = PrepaidProduct.objects.order_by(
-                filter_name).values_list(filter_name, flat=True).distinct()
+            available_filter_options[filter_name] = \
+                get_available_filter_options(all_products, filter_name)
 
     current_count = products.count()
     paginator = Paginator(products.all(), 20)
     page_number = validate_page_number(request, paginator)
-    page = paginator.page(page_number)
+    page_results = paginator.page(page_number)
 
     return render(request, 'prepaid_agreements/index.html', {
-        'current_page': page_number,
-        'results': page,
-        'total_count': total_count,
-        'paginator': paginator,
+        'applied_filters': filters,
+        'available_filter_options': available_filter_options,
         'current_count': current_count,
-        'filters': params,
+        'current_page': page_number,
+        'paginator': paginator,
         'query': search_term or '',
-        'active_filters': available_filters,
-        'valid_filters': valid_filters,
+        'results': page_results,
         'search_field': search_field,
+        'total_count': total_count,
     })
 
 
