cd281211bba1db9e6859a11b3469607ce840cca5
Mike Green <mike.green@simplethread.com>
2020-03-17 10:43:29 -0400

    Credit where credit is due

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/number-helpers.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/number-helpers.js
@@ -6,6 +6,8 @@
  * @param {number} max The maximum value in the input range
  * @param {number} outMin The minimum value in the output range
  * @param {number} outMax The maximum value in the output range
+ * @returns {number}
+ * @see {@link https://stackoverflow.com/questions/345187/math-mapping-numbers}
  */
 export const mapRange = (num, min, max, outMin, outMax) => (num - min) / (max - min) * (outMax - outMin) + outMin;
 
@@ -15,5 +17,6 @@ export const mapRange = (num, min, max, outMin, outMax) => (num - min) / (max - 
  * @param {number} num The number to clamp
  * @param {number} min The minimum allowable value
  * @param {number} max The maximum allowable value
+ * @see {@link https://stackoverflow.com/questions/11409895/whats-the-most-elegant-way-to-cap-a-number-to-a-segment}
  */
 export const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
