2516fafc904ce64bf1615701aabfa3b04b2d7c2d
Mike Green <mike.green@simplethread.com>
2020-04-06 11:48:05 -0400

    Refactor conditionals in fix-it strategy page component to DRY it up

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/strategies/fix-it/index.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/strategies/fix-it/index.js
@@ -7,7 +7,25 @@ import { useScrollToTop } from '../../../components/scroll-to-top';
 import { dayjs } from '../../../lib/calendar-helpers';
 import { ButtonLink } from '../../../components/button';
 
-import { arrowLeft, ideaRound } from '../../../lib/icons';
+import { pencil, arrowLeft, ideaRound } from '../../../lib/icons';
+
+const StrategyCards = ({ results }) => (
+  <main className="strategies-cards">
+    <CardGroup columns={2}>
+      {results.map((result, index) => (
+        <Card title={result.title} icon={ideaRound} key={`strategy-${index}`}>
+          <p>{result.text}</p>
+
+          <div className="m-card_footer">
+            <ButtonLink icon={pencil} to={`/calendar/add/${result.event.id}/edit`} variant="secondary">
+              Edit {result.event.categoryDetails.name}
+            </ButtonLink>
+          </div>
+        </Card>
+      ))}
+    </CardGroup>
+  </main>
+);
 
 function FixItStrategies() {
   const { uiStore, strategiesStore: strategies } = useStore();
@@ -21,62 +39,34 @@ function FixItStrategies() {
 
     const weekInt = Number(week);
 
-    if (weekInt && weekInt !== uiStore.currentWeek.valueOf())
-    uiStore.setCurrentWeek(dayjs(weekInt));
+    if (weekInt && weekInt !== uiStore.currentWeek.valueOf()) uiStore.setCurrentWeek(dayjs(weekInt));
   }, []);
 
   useScrollToTop();
-
-  if (!strategies.strategyResults.length) {
-    return (
-      <section className="strategies">
-        <header className="strategies-header">
-          <h2 className="strategies-header__title">
-            Fix-It Strategies
-          </h2>
-        </header>
-
-        <p>
-          <em>There are no strategy recommendations for this week</em>
-        </p>
-
-        <footer className="strategies-footer">
-          <ButtonLink iconSide="left" icon={arrowLeft} to="/calendar">Back to Calendar</ButtonLink>
-        </footer>
-      </section>
-    );
-  }
 
   return (
     <section className="strategies">
       <header className="strategies-header">
         <h2 className="strategies-header__title">Fix-It Strategies</h2>
 
-        <p className="strategies-header__intro">
-          This week you went into the red. Below are the transactions you made. Explore the different strategies that can prevent going into the red in the future.
-        </p>
+        {strategies.fixItResults.length ? (
+          <p className="strategies-header__intro">
+            This week you went into the red. Below are the transactions you made. Explore the different strategies that
+            can prevent going into the red in the future.
+          </p>
+        ) : (
+          <p>
+            <em>There are no strategy recommendations for this week</em>
+          </p>
+        )}
       </header>
 
-      <main className="strategies-cards">
-        <CardGroup columns={2}>
-          {strategies.strategyResults.map((result, index) => (
-            <Card title={result.title} icon={ideaRound} key={`strategy-${index}`}>
-              <p>
-                {result.text}
-              </p>
-
-              <div className="m-card_footer">
-                <ButtonLink to={`/calendar/add/${result.event.id}/edit`} variant="secondary">
-                  Edit {result.event.categoryDetails.name}
-                </ButtonLink>
-              </div>
-            </Card>
-          ))}
-        </CardGroup>
-      </main>
+      {strategies.fixItResults.length > 0 && <StrategyCards results={strategies.fixItResults} />}
 
       <footer className="strategies-footer">
-        <ButtonLink iconSide="left" icon={arrowLeft} to="/calendar">Back to Calendar</ButtonLink>
+        <ButtonLink iconSide="left" icon={arrowLeft} to="/calendar">
+          Back to Calendar
+        </ButtonLink>
       </footer>
     </section>
   );
